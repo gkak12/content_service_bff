ed80cc5d4a7f81ad8d767b7d3c107d278e9dffda
gkak12 <hyunjun5681@kakao.com>
2025-07-08 21:59:38 +0900

    ErrorCodeEnum 수정

--- module_common/src/main/kotlin/com/service/common/enums/ErrorCodeEnum.kt
+++ module_common/src/main/kotlin/com/service/common/enums/ErrorCodeEnum.kt
@@ -1,6 +1,6 @@
 package com.service.common.enums
 
-enum class ErrorCodeEnum (val value: String, val msg: String, val code: Int) {
+enum class ErrorCodeEnum (val value: String, val msg: String, val status: Int) {
     VALIDATION_CHECK("VALIDATION_CHECK", "검증에 실패하였습니다.", 400),
     UNAUTHENTICATED("UNAUTHENTICATED", "인증되지 않은 사용자입니다.", 401),
     PERMISSION_DENIED("PERMISSION_DENIED", "권한이 없는 사용자입니다.", 403),
@@ -8,4 +8,5 @@ enum class ErrorCodeEnum (val value: String, val msg: String, val code: Int) {
     METHOD_NOT_ALLOWED("METHOD_NOT_ALLOWED", "지원하지 않는 HTTP 메소드입니다.", 405),
     ALREADY_EXIST("ALREADY_EXIST", "이미 존재하는 리소스입니다.", 409),
     RESOURCE_GONE("RESOURCE_GONE", "이미 삭제된 리소스입니다.", 410),
+    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버에서 알 수 없는 오류가 발생하였습니다.", 500)
 }
