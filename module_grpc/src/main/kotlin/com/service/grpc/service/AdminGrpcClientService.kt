7c50746b9560f269259668e1723207f4f3b02be0
gkak12 <hyunjun5681@kakao.com>
2025-07-10 01:48:34 +0900

    grpc 모듈 Admin 클라이언트 코드 추가

---
+++ module_grpc/src/main/kotlin/com/service/grpc/service/AdminGrpcClientService.kt
@@ -0,0 +1,23 @@
+package com.service.grpc.service
+
+import com.service.account.GrpcAdminRequest
+import com.service.account.GrpcAdminResponse
+import com.service.account.GrpcAdminServiceGrpc
+import io.grpc.Channel
+import net.devh.boot.grpc.client.inject.GrpcClient
+import org.springframework.stereotype.Service
+
+@Service
+class AdminGrpcClientService {
+
+    @GrpcClient("admin-service")
+    private lateinit var adminServiceChannel: Channel
+
+    private fun adminStub(): GrpcAdminServiceGrpc.GrpcAdminServiceBlockingStub {
+        return GrpcAdminServiceGrpc.newBlockingStub(adminServiceChannel)
+    }
+
+    fun findAdminByName(request: GrpcAdminRequest): GrpcAdminResponse {
+        return adminStub().findAdminByName(request)
+    }
+}
