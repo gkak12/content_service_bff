9db164647cd3cf742e4223f9bf4a0ae7d8251ecb
gkak12 <hyunjun5681@kakao.com>
2025-07-23 20:32:43 +0900

    Jwt 토큰 권한 추가

--- service_admin/src/main/kotlin/com/service/admin/security/JwtUtil.kt
+++ service_admin/src/main/kotlin/com/service/admin/security/JwtUtil.kt
@@ -1,5 +1,6 @@
 package com.service.admin.security
 
+import com.service.account.GrpcAdminProtoDto
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
@@ -20,8 +21,9 @@ class JwtUtil(
     private val secretKey: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
 
     // Access/Refresh Token 생성
-    fun createToken(flag:String, userId: String): String {
-        val claims: Claims = Jwts.claims().setSubject(userId) // Token에 사용자 아이디 추가
+    fun createToken(flag:String, protoDto: GrpcAdminProtoDto): String {
+        val claims: Claims = Jwts.claims().setSubject(protoDto.adminId) // Token에 사용자 아이디 추가
+        claims["ROLE"] = protoDto.adminType
         val now = Date()
 
         // 토큰 타입에 따라 만료 시간 설정
@@ -35,8 +37,13 @@ class JwtUtil(
             .compact()
     }
 
-    // Token에서 사용자 아이디 추출
-    fun getUsername(token: String): String {
+    // Token 유효성 검증
+    fun validateToken(token: String, adminId: String, adminType: String): Boolean {
+        return !isTokenExpired(token) && adminId == getAdminId(token) && adminType == getAdminRole(token)
+    }
+
+    // Token에서 아이디 추출
+    fun getAdminId(token: String): String {
         return Jwts.parserBuilder()
             .setSigningKey(secretKey)
             .build()
@@ -45,7 +52,16 @@ class JwtUtil(
             .subject
     }
 
-    // Token 유효성 검사
+    // Token에서 권한 추출
+    fun getAdminRole(token: String): String {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body["ROLE"] as String
+    }
+
+    // Token 만료시간 검사
     fun isTokenExpired(token: String): Boolean {
         val expiration = Jwts.parserBuilder()
             .setSigningKey(secretKey)
@@ -55,10 +71,5 @@ class JwtUtil(
             .expiration
 
         return expiration.before(Date())
-    }
-
-    // Token 검증
-    fun validateToken(token: String, userId: String): Boolean {
-        return userId == getUsername(token) && !isTokenExpired(token)
     }
 }
