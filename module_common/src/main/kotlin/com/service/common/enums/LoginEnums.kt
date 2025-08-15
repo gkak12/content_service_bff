1fbeda5e6e893b8b19b0dccf694329d86c25aa61
gkak12 <hyunjun5681@kakao.com>
2025-08-15 23:37:33 +0900

    LoginEnums 수정

--- module_common/src/main/kotlin/com/service/common/enums/LoginEnums.kt
+++ module_common/src/main/kotlin/com/service/common/enums/LoginEnums.kt
@@ -1,6 +1,7 @@
 package com.service.common.enums
 
 enum class LoginEnums(val value: String, val msg: String) {
-    NAVER_OAUTH_EMAIL("email", "네이버 OAUTH 계정 이메일"),
-    NAVER_OAUTH_NAME("name", "네이버 OAUTH 계정 이름")
+    NAVER_OAUTH2("naver", "네이버 OAUTH2 로그인"),
+    NAVER_OAUTH2_EMAIL("email", "네이버 OAUTH2 계정 이메일"),
+    NAVER_OAUTH2_NAME("name", "네이버 OAUTH2 계정 이름")
 }
