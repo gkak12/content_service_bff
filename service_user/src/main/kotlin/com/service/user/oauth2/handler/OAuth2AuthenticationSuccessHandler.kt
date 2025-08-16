342f38fc1a63df4d948ab2ab8ee282e783ef24c8
gkak12 <hyunjun5681@kakao.com>
2025-08-16 21:49:27 +0900

    네이버 OAuth2 연계 SuccessHandler 수정

--- service_user/src/main/kotlin/com/service/user/oauth2/handler/OAuth2AuthenticationSuccessHandler.kt
+++ service_user/src/main/kotlin/com/service/user/oauth2/handler/OAuth2AuthenticationSuccessHandler.kt
@@ -1,7 +1,12 @@
 package com.service.user.oauth2.handler
 
+import com.service.account.GrpcUserProtoDto
 import com.service.common.enums.LoginEnums
+import com.service.common.response.JwtResponse
+import com.service.grpc.service.GrpcClientUserService
+import com.service.user.model.mapper.UserMapper
 import com.service.user.oauth2.service.OAuth2Service
+import com.service.user.security.JwtUtil
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -14,7 +19,9 @@ import java.util.*
 
 @Component
 class OAuth2AuthenticationSuccessHandler(
-    private val oAuth2Service: OAuth2Service
+    private val jwtUtil: JwtUtil,
+    private val userMapper: UserMapper,
+    private val grpcClientUserService: GrpcClientUserService
 ): ServerAuthenticationSuccessHandler {
 
     private val logger = LoggerFactory.getLogger(OAuth2AuthenticationSuccessHandler::class.java)
@@ -29,21 +36,35 @@ class OAuth2AuthenticationSuccessHandler(
         val userEmail = principal.getAttribute<String>(LoginEnums.NAVER_OAUTH2_EMAIL.value)
         val userName = principal.getAttribute<String>(LoginEnums.NAVER_OAUTH2_NAME.value)
 
-        logger.info("email: ${userEmail}")
+        logger.info("email: ${userEmail}, name: ${userName}")
 
         // 비동기적으로 사용자 정보를 저장한 후 리다이렉트
-        return oAuth2Service.loginUser(userEmail, userName)
-            .then(redirectToUserInfo(webFilterExchange.exchange, userEmail))
-    }
+        return grpcClientUserService.loginUser(
+                GrpcUserProtoDto.newBuilder()
+                    .setUserId(userEmail)
+                    .setUserName(userName)
+                    .setUserLoginType(LoginEnums.NAVER_OAUTH2.value)
+                    .build()
+            )
+            .flatMap { response ->
+                val userDto = userMapper.toDto(response.dtoList[0])
+                val accessToken = jwtUtil.createToken("access", userDto)
+                val refreshToken = jwtUtil.createToken("refresh", userDto)
+                val jwtResponse = JwtResponse(accessToken, refreshToken, "${userDto.userId}: OAuth2 로그인 성공했습니다.")
 
-    private fun redirectToUserInfo(exchange: ServerWebExchange, userEmail: String): Mono<Void> {
-        val encodedEmail = Base64.getEncoder().encodeToString(userEmail.toByteArray())
-        val location = "/api/user/oauth2/me?id=$encodedEmail"
+                // 응답 객체에 직접 JSON 작성
+                val exchange = webFilterExchange.exchange
+                val responseObj = exchange.response
+                val bufferFactory = responseObj.bufferFactory()
 
-        val response = exchange.response
-        response.statusCode = org.springframework.http.HttpStatus.FOUND // 302 Redirect
-        response.headers.location = java.net.URI.create(location)
+                val objectMapper = com.fasterxml.jackson.databind.ObjectMapper()
+                val bytes = objectMapper.writeValueAsBytes(jwtResponse)
+                val buffer = bufferFactory.wrap(bytes)
 
-        return response.setComplete()
+                responseObj.headers.contentType = org.springframework.http.MediaType.APPLICATION_JSON
+                responseObj.statusCode = org.springframework.http.HttpStatus.OK
+
+                responseObj.writeWith(Mono.just(buffer))
+            }
     }
 }
