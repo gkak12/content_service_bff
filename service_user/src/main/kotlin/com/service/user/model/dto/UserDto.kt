4e4d4e02d996a0704801b7a422fd30134d660c5c
gkak12 <hyunjun5681@kakao.com>
2025-08-16 21:25:23 +0900

    UserDto 수정

--- service_user/src/main/kotlin/com/service/user/model/dto/UserDto.kt
+++ service_user/src/main/kotlin/com/service/user/model/dto/UserDto.kt
@@ -1,10 +1,10 @@
 package com.service.user.model.dto
 
-class UserDto(
+data class UserDto(
     var userId: String = "",
     var userPassword: String = "",
+    var userLoginType: String = "",
     var userName: String = "",
     var userEmail: String = "",
-    var userAddress: String = "",
-    var userLoginType: String = "",
+    var userAddress: String = ""
 )
