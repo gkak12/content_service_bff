5cd049c9133b4cd15142037ceb211382fae69cff
gkak12 <hyunjun5681@kakao.com>
2025-08-18 00:31:37 +0900

    whitelist 수정

--- service_user/src/main/kotlin/com/service/user/security/UserAuthWhitelist.kt
+++ service_user/src/main/kotlin/com/service/user/security/UserAuthWhitelist.kt
@@ -4,7 +4,7 @@ object UserAuthWhitelist {
     val paths = arrayOf(
         "/api/user/login",
         "/api/user/signup",
-        "/api/user/oauth2/me",
+        "/api/user/oauth2/me/**",
         "/login/oauth2/**",
         "/oauth2/authorization/**",
         "/api/user/jwt/refresh"
